27ce336c16b2f241f2f28f1407b327c00f23ae13
BiscuitPrime <henri.nomico@gmail.com>
2021-12-10 13:12:21 +0100

    New generate palette activity

---
+++ app/src/main/java/com/example/palettefinder/GeneratePalette.java
@@ -0,0 +1,14 @@
+package com.example.palettefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratePalette extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_palette);
+    }
+}
--- app/src/main/java/com/example/palettefinder/MainActivity.java
+++ app/src/main/java/com/example/palettefinder/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.palettefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button quitButton = (Button) findViewById(R.id.quitButton);
+        quitButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){finish();}
+        });
+
+        Button generateButton = (Button) findViewById(R.id.generateButton);
+        generateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent generateIntent = new Intent(MainActivity.this, GeneratePalette.class);
+                MainActivity.this.startActivity(generateIntent);
+            }
+        });
     }
 }
